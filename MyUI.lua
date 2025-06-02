6f41118ee97762e7ec524d49759b37bd208f09e7
Mykel Ruvola <159888+mykel242@users.noreply.github.com>
2025-06-02 10:09:50 -0400

    removed reference to otherwise unused file

--- MyUI.lua
+++ MyUI.lua
@@ -14,8 +14,8 @@ end
 addon.frame = CreateFrame("Frame")
 
 -- Development version tracking
-addon.VERSION = "main-ad537b3"
-addon.BUILD_DATE = "2025-06-01-20:33"
+addon.VERSION = "main-fbe715a"
+addon.BUILD_DATE = "2025-06-02-10:09"
 
 -- Debug flag (will be loaded from saved variables)
 addon.DEBUG = false
