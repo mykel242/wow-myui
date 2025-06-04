7856c8b19e638dc3d207f1cf0802d28266b9ed10
Mykel Ruvola <159888+mykel242@users.noreply.github.com>
2025-06-04 17:22:38 -0400

    added a packaging shell script

--- MyUI.lua
+++ MyUI.lua
@@ -14,8 +14,8 @@ end
 addon.frame = CreateFrame("Frame")
 
 -- Development version tracking
-addon.VERSION = "main-fbe715a"
-addon.BUILD_DATE = "2025-06-02-10:09"
+addon.VERSION = "main-6f41118"
+addon.BUILD_DATE = "2025-06-04-17:22"
 
 -- Debug flag (will be loaded from saved variables)
 addon.DEBUG = false
